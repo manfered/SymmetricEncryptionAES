574205b749cd358ae007eb9aac6d02dcf676e468
manfered <35757736+manfered@users.noreply.github.com>
2018-08-05 14:42:14 -0600

    Add project files.

---
+++ SymmetricEncryptionAES/AESEncryption.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace SymmetricEncryptionAES
+{
+    public class AESEncryption
+    {
+        public byte[] GenerateRandomNumber(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+
+        public byte[] Encrypt(byte[] toBeEnrypted, byte[] key, byte[] iv)
+        {
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+
+                    cryptoStream.Write(toBeEnrypted, 0, toBeEnrypted.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+
+        public byte[] Decrypt(byte[] toBeDecrypted, byte[] key, byte[] iv)
+        {
+            using(var aes = new AesCryptoServiceProvider())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Write);
+
+                    cryptoStream.Write(toBeDecrypted, 0, toBeDecrypted.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ SymmetricEncryptionAES/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymmetricEncryptionAES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aes = new AESEncryption();
+            var key = aes.GenerateRandomNumber(32);
+            var iv = aes.GenerateRandomNumber(16);
+
+            const string message = "Message to Encrypt";
+
+            var encrypted = aes.Encrypt(Encoding.UTF8.GetBytes(message), key, iv);
+            var decrypted = aes.Decrypt(encrypted, key, iv);
+
+            var decryptedMessage = Encoding.UTF8.GetString(decrypted);
+
+            Console.WriteLine("AES Encryption");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"Message text : {message}");
+            Console.WriteLine($"Encryption Text = {Convert.ToBase64String(encrypted)}");
+            Console.WriteLine($"Decrypted Text = {decryptedMessage}");
+
+        }
+    }
+}
